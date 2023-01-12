4e5ad3ce98108f7d8368ee31188ffbd126c89a2e
zhuicode99 <enlin291814833@hotmail.com>
2023-01-12 17:08:55 -0500

    add the second template

--- express_server.js
+++ express_server.js
@@ -32,8 +32,12 @@ app.get("/urls",(req, res) => {
   res.render("urls_index", templateVars); 
 });
 
-
-
+app.get("/urls/:id",(req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});//id is a route parameter. 
+//This means that the value in this part of the url 
+//will be available in the req.params object.
 
 
 
