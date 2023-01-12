53e4485680d460aafdf61655d1935d6604dd0423
zhuicode99 <enlin291814833@hotmail.com>
2023-01-12 16:53:25 -0500

    add the first template ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+app.set("view engine", "ejs") // set the structure 
+
+
+
 const urlDatabase = { //temp database
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +15,33 @@ const urlDatabase = { //temp database
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls.json",(req, res) => {
+   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.get("/urls",(req, res) => {
+  const templateVars = {
+    urls: urlDatabase
+  }
+  res.render("urls_index", templateVars); 
+});
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
