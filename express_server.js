4f1e3deab4c7ee1410b2ef9012bc56aade480c0d
zhuicode99 <enlin291814833@hotmail.com>
2023-01-13 16:48:02 -0500

    add new endpoint to redirect to longURL using shortID

--- express_server.js
+++ express_server.js
@@ -44,16 +44,29 @@ app.post("/urls", (req, res) => {
   urlDatabase[id] = req.body['longURL'];
   res.redirect(`/urls/${id}`)
 });
+//从urls—new.ejs 里面的form输入long-url后点submit，由于form的action是/urls
+//所以会post（create）到/urls页面，这个end point就是完成post到/url的操作，
+//用function生成的shortid，添加到urldatabase，然后完成post到/url界面这个action以后直接跳转
+//跳转到/urls/：id 界面，而不是停留在/url界面。但是数据更新在了/url界面。
 //new
 
 app.get("/urls/:id",(req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); 
 })//like /urls/b2xVn2 in the browser. 
 //Further, the value of req.params.id would be b2xVn2.
 
-
-
+//use shortID link to redirect to longID link
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  let http = 'http://';
+  if(longURL.includes(http)) {
+    res.redirect(`${longURL}`);
+  }
+  else {
+    res.redirect(`${http}${longURL}`); //==》  res.redirect(`http://${longURL}`)
+  }
+});
 
 
 
